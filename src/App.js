2fbbc4d8c109e3970a8919bb79588c7253bc2243
Stephanie Ugboaja <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2023-11-11 00:06:41 +0000

    Create and style dynamic list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import Profile from "./PropsChallenge/Profile";
 import Props from "./props";
 import "./styles.css";
 
 export default function App() {
+  const data = ["React 🌙", "Next 🌙"];
   return (
     <div className="App">
       <Props
         name={{
           ingredients: "salt",
-          aka: "tc"
+          aka: "tc",
         }}
         age={9}
+      />
+      <Profile
+        name={"Uchechi Ugboaja"}
+        about={"I'm a frontend developer"}
+        skills={data}
       />
     </div>
   );
---
+++ src/PropsChallenge/Profile.js
@@ -0,0 +1,33 @@
+const Profile = ({ name, about, skills }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{about}</p>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index} style={{ backgroundColor: getColor(index) }}>
+            {skill}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const getColor = (index) => {
+  const colors = ["blue", "red"];
+  return colors[index];
+  // ChatGpt Solution
+  // index % colors.length: The modulo operator calculates
+  // the remainder when index is divided by colors.length.
+  // This operation ensures that the index stays within the
+  // bounds of the array.
+
+  // colors[index % colors.length]: This retrieves the color
+  // from the colors array at the calculated index. If the index
+  // exceeds the length of the colors array, it wraps around to
+  // the beginning of the array, creating a cycling effect.
+  //return colors[index % colors.length]
+};
+
+export default Profile;
